a8b5ffaa8d9b7b918347f6830aa1e382b3ed323f
root <root@DESKTOP-CIT8OGS.localdomain>
2020-06-28 17:28:54 -0700

    First iteration

---
+++ adobe_renewer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import os
+import re
+import sys
+
+#os.chdir('/mnt/c/Program Files/Adobe/Adobe Illustrator CC 2018/Support Files/Contents/Windows/AMT/')
+
+input_fh = open('/mnt/c/Program Files/Adobe/Adobe Illustrator CC 2018/Support Files/Contents/Windows/AMT/application.xml', 'r')
+output_fh = open('/mnt/c/Program Files/Adobe/Adobe Illustrator CC 2018/Support Files/Contents/Windows/AMT/application_new.xml', 'w')
+
+i = 0
+for line in input_fh.readlines():
+    i += 1
+    if 'TrialSerialNumber' in line:
+
+        match = re.search('TrialSerialNumber">(\d+)<', line)
+        if match:
+            id = match.group(1)
+            id = int(id) + 1
+            print("New TrialSerialNumber ID is %s." % id)
+        else:
+            print("Error: Found TrialSerialNumber but not matching ID.")
+            sys.exit(1)
+
+        line = re.sub('TrialSerialNumber">\d+<', 'TrialSerialNumber">%s<' % id, line)
+
+    output_fh.write(line)
+os.remove('/mnt/c/Program Files/Adobe/Adobe Illustrator CC 2018/Support Files/Contents/Windows/AMT/application.xml')
+os.rename('/mnt/c/Program Files/Adobe/Adobe Illustrator CC 2018/Support Files/Contents/Windows/AMT/application_new.xml', '/mnt/c/Program Files/Adobe/Adobe Illustrator CC 2018/Support Files/Contents/Windows/AMT/application.xml')
+
